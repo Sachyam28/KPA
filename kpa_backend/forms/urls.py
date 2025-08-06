94d0035b59f7b3bff9f0f8ed2ec206c1e763d48c
Sachyam28 <sachyamshakya@gmail.com>
2025-08-06 16:44:07 +0530

    final changes

--- kpa_backend/forms/urls.py
+++ kpa_backend/forms/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import create_wheel_spec, create_bogie_checksheet, get_wheel_spec_filtered
+from .views import create_wheel_spec, create_bogie_checksheet
 
 urlpatterns = [
     path('forms/wheel-specifications', create_wheel_spec),
     path('forms/bogie-checksheet', create_bogie_checksheet),
-    path('forms/wheel-specifications/filter/', get_wheel_spec_filtered),  # for GET with filters
 ]
--- kpa_backend/forms/views.py
+++ kpa_backend/forms/views.py
@@ -44,25 +44,3 @@ def create_bogie_checksheet(request):
         }, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(['GET'])
-def get_wheel_spec_filtered(request):
-    formNumber = request.GET.get('formNumber')
-    submittedBy = request.GET.get('submittedBy')
-    submittedDate = request.GET.get('submittedDate')
-
-    filters = {}
-    if formNumber:
-        filters['formNumber'] = formNumber
-    if submittedBy:
-        filters['submittedBy'] = submittedBy
-    if submittedDate:
-        filters['submittedDate'] = submittedDate
-
-    qs = WheelSpecification.objects.filter(**filters)
-    serializer = WheelSpecificationSerializer(qs, many=True)
-    return Response({
-        "success": True,
-        "message": "Filtered wheel specification forms fetched successfully.",
-        "data": serializer.data
-    })
